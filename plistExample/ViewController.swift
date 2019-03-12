4d580bda68d87a399d6d2695672ef625185b778e
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-03-12 19:06:33 -0400

    Initial Commit

---
+++ plistExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  plistExample
+//
+//  Created by Baturay Kayatürk on 2019-03-12.
+//  Copyright © 2019 Lambton College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
