ae4f1c539d6b4db097fae7388d8fd45f25f939dc
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-03-13 18:19:13 -0400

    plist example. only reading.

--- plistExample/ViewController.swift
+++ plistExample/ViewController.swift
@@ -12,7 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.readInformationPlist()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func readInformationPlist() {
+        if let bundlePath = Bundle.main.path(forResource: "CountryList", ofType: "plist") {
+            let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+            
+            print(dictionary!.description)
+            
+            let countryList = dictionary!["countries"] as! NSArray
+                print(countryList[0])
+                for c in countryList {
+                    print(c)
+                }
+            
+            let employeeList = dictionary!["employees"] as! NSArray
+            for e in employeeList {
+                let emp = e as! NSDictionary
+                print("eid : \(emp["eid"]!)")
+                print("name : \(emp["name"]!)")
+                print("salary : \(emp["salary"]!)")
+            }
+            
+        }
     }
 
 
